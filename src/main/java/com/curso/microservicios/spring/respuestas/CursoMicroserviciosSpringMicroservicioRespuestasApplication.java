13ec7ed110f65efa6c3426e2f143bfd093bca5a3
Alfredo Alas <alfredo.alas14@gmail.com>
2024-09-02 14:54:21 -0600

    add @EntityScan

--- src/main/java/com/curso/microservicios/spring/respuestas/CursoMicroserviciosSpringMicroservicioRespuestasApplication.java
+++ src/main/java/com/curso/microservicios/spring/respuestas/CursoMicroserviciosSpringMicroservicioRespuestasApplication.java
@@ -2,10 +2,14 @@ package com.curso.microservicios.spring.respuestas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @EnableEurekaClient
 @SpringBootApplication
+@EntityScan({"com.curso.microservicios.spring.respuestas.models.entity",
+	"com.curso.microservicios.spring.generic.alumnos.models.entity",
+	"com.curso.microservicios.spring.generic.examenes.models.entity"})
 public class CursoMicroserviciosSpringMicroservicioRespuestasApplication {
 
 	public static void main(String[] args) {
