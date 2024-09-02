e07ddf71947fa203bc67ac940729a1034bdabf9e
Alfredo Alas <alfredo.alas14@gmail.com>
2024-09-02 14:27:53 -0600

    add project files

---
+++ src/main/java/com/curso/microservicios/spring/respuestas/CursoMicroserviciosSpringMicroservicioRespuestasApplication.java
@@ -0,0 +1,13 @@
+package com.curso.microservicios.spring.respuestas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoMicroserviciosSpringMicroservicioRespuestasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoMicroserviciosSpringMicroservicioRespuestasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/curso/microservicios/spring/respuestas/CursoMicroserviciosSpringMicroservicioRespuestasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.microservicios.spring.respuestas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoMicroserviciosSpringMicroservicioRespuestasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
