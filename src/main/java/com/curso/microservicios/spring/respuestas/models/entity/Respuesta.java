f70fd57ee6141d0e757935b2ccd1e54b64652a74
Alfredo Alas <alfredo.alas14@gmail.com>
2024-09-02 15:00:09 -0600

    add relationship to Alumno and Examen

--- src/main/java/com/curso/microservicios/spring/respuestas/models/entity/Respuesta.java
+++ src/main/java/com/curso/microservicios/spring/respuestas/models/entity/Respuesta.java
@@ -2,10 +2,16 @@ package com.curso.microservicios.spring.respuestas.models.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
+
+import com.curso.microservicios.spring.generic.alumnos.models.entity.Alumno;
+import com.curso.microservicios.spring.generic.examenes.models.entity.Pregunta;
 
 @Entity
 @Table(name = "respuestas")
@@ -17,6 +23,12 @@ public class Respuesta {
 	
 	@Column(name = "texto")
 	private String texto;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	private Alumno alumno;
+	
+	@OneToOne(fetch = FetchType.LAZY)
+	private Pregunta pregunta;
 
 	public Long getId() {
 		return id;
@@ -32,6 +44,22 @@ public class Respuesta {
 
 	public void setTexto(String texto) {
 		this.texto = texto;
+	}
+
+	public Alumno getAlumno() {
+		return alumno;
+	}
+
+	public void setAlumno(Alumno alumno) {
+		this.alumno = alumno;
+	}
+
+	public Pregunta getPregunta() {
+		return pregunta;
+	}
+
+	public void setPregunta(Pregunta pregunta) {
+		this.pregunta = pregunta;
 	}
 	
 }
