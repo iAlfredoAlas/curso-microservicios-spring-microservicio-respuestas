7488fad623f75a6e5dddc7cfd833039df73234cc
Alfredo Alas <alfredo.alas14@gmail.com>
2024-09-10 14:21:38 -0600

    add method findRespuestaByAlumnoByExamen

--- src/main/java/com/curso/microservicios/spring/respuestas/services/RespuestaServiceImpl.java
+++ src/main/java/com/curso/microservicios/spring/respuestas/services/RespuestaServiceImpl.java
@@ -12,10 +12,17 @@ public class RespuestaServiceImpl implements RespuestaService {
 
 	@Autowired
 	private RespuestaRepository respuestaRepository;
+
 	@Override
 	@Transactional
 	public Iterable<Respuesta> saveAll(Iterable<Respuesta> respuestas) {
 		return respuestaRepository.saveAll(respuestas);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public Iterable<Respuesta> findRespuestaByAlumnoByExamen(Long alumnoId, Long examenId) {
+		return respuestaRepository.findRespuestaByAlumnoByExamen(alumnoId, examenId);
 	}
 
 }
