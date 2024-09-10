d465644aa3c3112477b26593785b18b1afe93839
Alfredo Alas <alfredo.alas14@gmail.com>
2024-09-10 14:50:09 -0600

    add config to use dotenv

--- src/main/java/com/curso/microservicios/spring/respuestas/CursoMicroserviciosSpringMicroservicioRespuestasApplication.java
+++ src/main/java/com/curso/microservicios/spring/respuestas/CursoMicroserviciosSpringMicroservicioRespuestasApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @EnableEurekaClient
 @SpringBootApplication
 @EntityScan({"com.curso.microservicios.spring.respuestas.models.entity",
@@ -13,6 +15,10 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 public class CursoMicroserviciosSpringMicroservicioRespuestasApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(entry -> {
+            System.setProperty(entry.getKey(), entry.getValue());
+        });
 		SpringApplication.run(CursoMicroserviciosSpringMicroservicioRespuestasApplication.class, args);
 	}
 
